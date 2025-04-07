33f5400d36922e4f2406bf7eb6e0d539ef507bd4
Eygon Saldanha <eygon11@gmail.com>
2025-04-07 18:16:00 -0300

    ajuste em README.md

--- student-manager-frontend/src/app/app.routes.ts
+++ student-manager-frontend/src/app/app.routes.ts
@@ -6,13 +6,12 @@ import { AlunoComponent } from './features/aluno/aluno.component';
 import { AlunoFormComponent } from './features/aluno-form/aluno-form.component';
 
 export const routes: Routes = [
-  {path: '', component: PrivateRoutesComponent, children: [ 
-      { path: 'menu',  component: MenuComponent },
-      { path: 'aluno',  component: AlunoComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: PrivateRoutesComponent, children: [ 
+      { path: 'menu', component: MenuComponent },
+      { path: 'aluno', component: AlunoComponent },
       { path: 'aluno-form', component: AlunoFormComponent },
     ]
   },
-  { path: 'login',  component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' },
 ];
